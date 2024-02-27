ede03daa26f136c2d8217161acca7b7599bd31c2
Eric Ogbekene <ericogbekene@icloud.com>
2024-02-27 11:59:53 -0800

    puppet master script

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,8 +1,6 @@
 # Manifest file to install flask
-package { 'flask':
-  ensure	=> '2.1.0',
-}
 
-#exec { 'apt-get update':
-#command => 'usr/bin/apt-get update'
-#}
+package { 'flask':
+  ensure	=> 'installed',
+  require	=> Exec['apt-get update']
+}
