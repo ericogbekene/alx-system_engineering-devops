7940c68353e7725f2a45edc6ad9ff623bfe287a5
Eric Ogbekene <ericogbekene@icloud.com>
2024-04-21 14:56:20 +0100

    0-the_sky_is_the_limit_not.pp

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -10,17 +10,17 @@ exec { '/etc/nginx/nginx.conf_1':
   command => "sed -i 's/worker_connections.*/worker_connections 4000;/g' /etc/nginx/nginx.conf",
 }
 
--> exec { '/etc/nginx/nginx.conf_2':
+exec { '/etc/nginx/nginx.conf_2':
   path    => ['usr/local/sbin:usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin'],
   command => "sed -i 's/keepalive_timeout.*/keepalive_timeout 15;/g' /etc/nginx/nginx.conf",
 }
 
--> exec { '/etc/nginx/nginx.conf_3':
+exec { '/etc/nginx/nginx.conf_3':
   path    => ['usr/local/sbin:usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin'],
   command => "sed -i 's/access_log.*/access_log off;/g' /etc/nginx/nginx.conf",
 }
 
--> exec { '/etc/nginx/nginx.conf_4':
+exec { '/etc/nginx/nginx.conf_4':
   path    => ['usr/local/sbin:usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin'],
   command => "sed -i '/error_log.*/a \\\topen_file_cache_errors on;' /etc/nginx/nginx.conf",
 }
@@ -30,12 +30,12 @@ exec { '/etc/nginx/nginx.conf_5':
   command => "sed -i '/error_log.*/a \\\topen_file_cache_min_uses 2;' /etc/nginx/nginx.conf",
 }
 
--> exec { '/etc/nginx/nginx.conf_6':
+exec { '/etc/nginx/nginx.conf_6':
   path    => ['usr/local/sbin:usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin'],
   command => "sed -i '/error_log.*/a \\\topen_file_cache_valid 30s;' /etc/nginx/nginx.conf",
 }
 
--> exec { '/etc/nginx/nginx.conf_7':
+exec { '/etc/nginx/nginx.conf_7':
   path    => ['usr/local/sbin:usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin'],
   command => "sed -i '/error_log.*/a \\\topen_file_cache max=200000 inactive=20s;' /etc/nginx/nginx.conf",
 }
