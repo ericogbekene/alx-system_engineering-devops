60e5e24d5d085c5389436e7239c512efe801e1e1
Eric Ogbekene <ericogbekene@icloud.com>
2024-04-21 15:17:55 +0100

    fixing puppet lint for sky is the limit

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,12 +1,12 @@
 # Setting up the amount of traffic that Nginx server can handle.
 # Increase the ULIMIT of the default file
 exec { 'setting_up_nginx':
-	command => 'sed -i "s/15/4096/" /etc/default/nginx',
-	path    => '/usr/local/bin/:/bin/'
-		}
+  command => 'sed -i "s/15/4096/" /etc/default/nginx',
+  path    => '/usr/local/bin/:/bin/'
+  }
 
 # Restart Nginx
 exec { 'nginx-restart':
-	command => 'nginx restart',
-	path    => '/etc/init.d/'
-		}
+  command => 'nginx restart',
+  path    => '/etc/init.d/'
+  }
